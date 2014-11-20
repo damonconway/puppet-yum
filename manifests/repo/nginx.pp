4a6a01cf98e90f14b280bc765cb586e01fc94bae
Damon Conway <damon.conway@alchemysystems.com>
2014-11-20 17:34:11 -0600

    Adding mirror_url param to yum::repo::nginx

--- manifests/repo/nginx.pp
+++ manifests/repo/nginx.pp
@@ -3,13 +3,25 @@
 # This class installs the nginx repo
 #
 class yum::repo::nginx (
-  $enabled = 1
+  $enabled    = 1,
+  $mirror_url = undef
 ) {
-  $osver = split($::operatingsystemrelease, '[.]')
+
+  if $mirror_url {
+    validate_re(
+      $mirror_url,
+      '^(?:https?|ftp):\/\/[\da-zA-Z-][\da-zA-Z\.-]*\.[a-zA-Z]{2,6}\.?(?:\/[\w~-]*)*$',
+      '$mirror must be a Clean URL with no query-string, a fully-qualified hostname and no trailing slash.'
+    )
+    $baseurl = $mirror_url
+  } else {
+    $osver   = split($::operatingsystemrelease, '[.]')
+    $baseurl = "http://nginx.org/packages/rhel/${osver[0]}/\$basearch/"
+  }
 
   yum::managed_yumrepo { 'nginx':
     descr    => 'Nginx official release packages',
-    baseurl  => "http://nginx.org/packages/rhel/${osver[0]}/\$basearch/",
+    baseurl  => $baseurl,
     enabled  => $enabled,
     gpgcheck => 0,
     priority => 1,
