3fc650fe1762174b2e59563d884b02d6eab0213b
Damon Conway <damon.conway@alchemysystems.com>
2014-11-20 17:28:00 -0600

    Adding mirror_url param to yum::repo::percona

--- manifests/repo/percona.pp
+++ manifests/repo/percona.pp
@@ -3,11 +3,24 @@
 # This class installs the Percona RPM Repository
 #
 class yum::repo::percona (
-  $enabled = 1
+  $enabled    = 1,
+  $mirror_url = undef
 ) {
+
+  if $mirror_url {
+    validate_re(
+      $mirror_url,
+      '^(?:https?|ftp):\/\/[\da-zA-Z-][\da-zA-Z\.-]*\.[a-zA-Z]{2,6}\.?(?:\/[\w~-]*)*$',
+      '$mirror must be a Clean URL with no query-string, a fully-qualified hostname and no trailing slash.'
+    )
+    $baseurl = $mirror_url
+  } else {
+    $baseurl = 'http://repo.percona.com/centos/$releasever/os/$basearch/'
+  }
+
   yum::managed_yumrepo { 'percona':
     descr          => 'Percona RPM Repository (http://www.percona.com/percona-lab.html)',
-    baseurl        => 'http://repo.percona.com/centos/$releasever/os/$basearch/',
+    baseurl        => $baseurl,
     enabled        => $enabled,
     gpgcheck       => 1,
     failovermethod => 'priority',
